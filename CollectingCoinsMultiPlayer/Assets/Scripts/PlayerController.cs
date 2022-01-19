c68bd2fda34a57320761734d08301df46f8f7cfc
Volcano <doniamohamedabdelrahim@gmail.com>
2022-01-19 13:59:39 +0200

    Basic Sync

--- CollectingCoinsMultiPlayer/Assets/Scripts/PlayerController.cs
+++ CollectingCoinsMultiPlayer/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class PlayerController : MonoBehaviour
 {
     float horizontalInput;
@@ -10,26 +10,34 @@ public class PlayerController : MonoBehaviour
     float movementSpeed = 10;
     Rigidbody rb;
     GameplayUI gameplayUIScript;
+
+    PhotonView view;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         gameplayUIScript = GameObject.Find("GameplayUI").GetComponent<GameplayUI>();
+        view = GetComponent<PhotonView>();
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-        rb.AddForce(Vector3.forward * verticalInput * movementSpeed);
-        rb.AddForce(Vector3.right * horizontalInput * movementSpeed);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Coin")
+        if (view.IsMine)
         {
-            Destroy(other.gameObject);
-            gameplayUIScript.UpdateScore();
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            rb.AddForce(Vector3.forward * verticalInput * movementSpeed);
+            rb.AddForce(Vector3.right * horizontalInput * movementSpeed);
         }
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    //{
+    //    //    if (other.gameObject.tag == "Coin")
+    //    //    {
+    //    //        Destroy(other.gameObject);
+    //    //        gameplayUIScript.UpdateScore();
+    //    //    }
+    //    //}
+    //}
 }
